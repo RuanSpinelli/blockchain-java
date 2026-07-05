2b4a131c9d160583d3d9f6ce32ed5dba52effff2
Ruan Spinelli <ruanspinelli@gmail.com>
2026-07-05 14:15:07 -0300

    Método main

--- src/main/java/com/ruan/App.java
+++ src/main/java/com/ruan/App.java
@@ -1,13 +1,48 @@
 package com.ruan;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.google.gson.GsonBuilder;
+import com.ruan.blockchain.Block;
+
+
+import java.util.ArrayList;
+
+
+public class App
 {
+    public static ArrayList<Block> blockChain = new ArrayList<Block>();
+
+    public static Boolean isChainValid(){
+        Block currentBlock;
+        Block previousBlock;
+
+        for(int i = 0; i< blockChain.size(); i++){
+            currentBlock = blockChain.get(i);
+            previousBlock = blockChain.get(i-1);
+
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current hashes not equal at "+i);
+                return false;
+            }
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous hashes not equal at"+i);
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        blockChain.add(new Block("Eu sou o primeiro bloco", "0"));
+        blockChain.add(new Block("Sou o segundo bloco",blockChain.get(blockChain.size()-1).hash));
+        blockChain.add(new Block("Sou o terceiro bloco",blockChain.get(blockChain.size()-1).hash));
+
+        String blockChainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockChain);
+        System.out.println(blockChainJson);
+
+
+
     }
+
 }
