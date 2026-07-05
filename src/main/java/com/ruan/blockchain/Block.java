f739373d388421bed18b2b3ae93ebeb32e2f909c
Ruan Spinelli <ruanspinelli@gmail.com>
2026-07-05 18:32:45 -0300

    Minerador
    Método responsável por fazer mineração nos blocos

--- src/main/java/com/ruan/blockchain/Block.java
+++ src/main/java/com/ruan/blockchain/Block.java
@@ -9,6 +9,7 @@ public class Block {
     public String previousHash;
     private String data;
     private long timeStamp;
+    private int nonce;
 
 
     public Block(String data, String previousHash) {
@@ -25,4 +26,15 @@ public class Block {
                         data);
         return calculatedHash;
     }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0','0');
+        while(!hash.substring(0,difficulty).equals(target)){
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block mined!!!: "+ hash);
+    }
+
+
 }
